4bfef2d156f693904c89f7829aae16a6d0377668
Erick-Mtz <er-ick258@hotmail.com>
2025-04-12 14:00:36 -0500

    feat: add proyect translator be

---
+++ src/config/api/Servidor.ts
@@ -0,0 +1,39 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import rutaApiTraductor from "../../app/route/RutaTraductor";
+import chalk from "chalk";
+
+
+class Servidor {
+  public app: express.Application;
+
+  constructor() {
+    this.app = express();
+    this.configuracion();
+
+    this.app.use("/api/v1", rutaApiTraductor);
+  }
+
+  public configuracion(): void {
+    this.app.set("PORT", process.env.PORT || 3321);
+    this.app.use(express.json({ limit: "50mb" }));
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(cors());
+    this.app.use(morgan("dev"));
+  }
+
+  public iniciar(): void {
+    this.app.listen(this.app.get("PORT"), () => {
+      console.log("Backend is running 🚀 in this PORT: ", this.app.get("PORT"));
+      console.log("ᕦ( ᴼ ڡ ᴼ )ᕤ");
+      console.log(
+        chalk.green.bold("🠆 "),
+        "Local: ",
+        chalk.cyan.underline(`http://localhost:${chalk.cyan.bold(`${this.app.get("PORT")}`)}`)
+      );
+    });
+  }
+}
+
+export default Servidor;
